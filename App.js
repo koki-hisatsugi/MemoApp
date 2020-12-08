75377b954a56ae5cac42e1f2630736105b4deecd
koki-hisatsugi <hisakoki.gigaleopa@gmail.com>
2020-12-09 08:16:21 +0900

    Hide firebase certificate

--- App.js
+++ App.js
@@ -11,18 +11,36 @@ import MemoEditScreen from './src/screens/MemoEditScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
+import firebase from 'firebase';
+import ENV from './env.json';
+
+const Config = {
+apiKey: ENV.FIREBASE_API_KEY,
+authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+projectId: ENV.FIREBASE_PRJ_ID,
+storageBucket: ENV.FIREBASE_STRAGE,
+messagingSenderId: ENV.FIREBASE_SENDER_ID,
+appId: ENV.FIREBASE_APP_ID,
+measurementId: ENV.FIREBASE_MEASUREMENT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(Config);
+// firebase.analytics();
+
 const App = createStackNavigator({
+  Login: { screen: LoginScreen },
+  Signup: { screen: SignupScreen },
   Home: { screen: MemoListScreen },
   MemoDetail: { screen: MemoDetailScreen },
   MemoEdit: { screen: MemoEditScreen },
-  Login: { screen: LoginScreen },
-  Signup: { screen: SignupScreen },
 }, {
   defaultNavigationOptions: {
     headerTitle: 'Memoooot',
     headerStyle:{
       backgroundColor: '#265366',
     },
+    headerTintColor:'#fff',
+    headerBackTitleStyle:null,
     headerTitleStyle:{
       color: '#fff',
       alignItems: 'center',
